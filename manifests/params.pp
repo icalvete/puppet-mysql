6d3eb3581d17753956d7e70c6d1a20ab57dc6fa3
Israel Calvete <icalvete@gmail.com>
2013-09-27 16:46:00 +0200

    Support for CentOS|RedHat

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,18 +1,28 @@
 class mysql::params {
 
+  $backup_dir      = hiera('backup_dir')
+  $mysql_root_pass = hiera('mysql_root_pass')
+
+  $mysql_server    = 'mysql-server'
+  $mysql_client    = 'mysql-client'
+
   case $::operatingsystem {
     /^(Debian|Ubuntu)$/: {
-      $backup_dir      = hiera('backup_dir')
-      $mysql_root_pass = hiera('mysql_root_pass')
 
       $mysql_conf      = '/etc/mysql/my.cnf'
-      $mysql_server    = 'mysql-server'
-      $mysql_client    = 'mysql-client'
       $mysql_dev       = 'libmysqlclient-dev'
       $mysql_service   = 'mysql'
 
     }
+    /^(CentOS|RedHat)$/: {
+
+      $mysql_conf    = '/etc/my.cnf'
+      $mysql_dev     = 'mysql-devel'
+      $mysql_service = 'mysqld'
+
+    }
     default: {
+
       fail ("${::operatingsystem} not supported.")
     }
   }
