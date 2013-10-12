bf4d35db518aa533e76a1a74e103d535471de9b9
Israel Calvete <icalvete@gmail.com>
2013-10-12 10:08:02 +0200

    New params to reppeat names in user and db

--- manifests/db.pp
+++ manifests/db.pp
@@ -1,18 +1,21 @@
 define mysql::db (
+
+  $db         = $name,
   $user       = 'root',
   $host       = 'localhost',
   $server     = 'localhost',
   $root_pass  = $mysql::params::mysql_root_pass,
   $privileges = 'all privileges',
+
 ){
   exec {
     "create_db_$name":
       path        => '/usr/bin:/bin:/sbin',
-      command     => "mysqladmin -uroot -p$root_pass -h \"$server\" create \"$name\"",
-      unless      => "mysql -uroot -p$root_pass -h \"$server\" -e 'use $name'";
+      command     => "mysqladmin -uroot -p$root_pass -h \"$server\" create \"$db\"",
+      unless      => "mysql -uroot -p$root_pass -h \"$server\" -e 'use $db'";
 
-    "set_privileges_$name":
+    "set_privileges_$db":
       path        => '/usr/bin:/bin:/sbin',
-      command     => "mysql -uroot -p$root_pass -h $server -e \"grant $privileges on $name.* to '$user'@'$host'\"",
+      command     => "mysql -uroot -p$root_pass -h $server -e \"grant $privileges on $db.* to '$user'@'$host'\"",
   }
 }
--- manifests/user.pp
+++ manifests/user.pp
@@ -1,5 +1,6 @@
 define mysql::user (
 
+  $user      = $name,
   $pass      = 'pass',
   $root_pass = $mysql::params::mysql_root_pass,
   $server    = 'localhost',
@@ -9,8 +10,8 @@ define mysql::user (
 
   exec {"add_mysql_user_${name}":
     path    => '/usr/bin:/bin:/sbin',
-    command => "mysql -uroot -p$root_pass -h \"$server\" -e \"create user '$name'@'$host' identified by '$pass'\"",
+    command => "mysql -uroot -p$root_pass -h \"$server\" -e \"create user '$user'@'$host' identified by '$pass'\"",
     require => Class[mysql::server::service],
-    unless  => "mysql -uroot -p$root_pass -h$server -e \"use mysql;select user,host from user;\"|grep $host|grep $name"
+    unless  => "mysql -uroot -p$root_pass -h$server -e \"use mysql;select user,host from user;\"|grep $host|grep $user"
   }
 }
