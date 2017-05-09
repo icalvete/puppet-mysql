d5f7b955622b9f762df22cc2164d7200d733f7a4
Israel Calvete <icalvete@gmail.com>
2017-05-09 19:14:16 +0200

    Add path to lense

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -3,4 +3,11 @@ class mysql::server::install {
   package {$mysql::params::mysql_server:
     ensure => present
   }
+
+  file {'augeas_mysql_len':
+    ensure  => present,
+    path    => '/usr/share/augeas/lenses/dist/mysql.aug',
+    content => template("${module_name}/mysql.aug.erb"),
+    mode    => '0664',
+  }
 }
