c4c478c84b2b8bebc1ebe826132dad6c568ca295
Israel Calvete <icalvete@gmail.com>
2019-02-18 09:14:33 +0100

    charset_name and collation_name are now parameters.

--- manifests/db.pp
+++ manifests/db.pp
@@ -1,12 +1,14 @@
 define mysql::db (
 
-  $db         = $name,
-  $user       = 'root',
-  $host       = 'localhost',
-  $server     = 'localhost',
-  $root_user  = $mysql::server::root_user,
-  $root_pass  = $mysql::server::root_pass,
-  $privileges = 'all privileges',
+  $db             = $name,
+  $user           = 'root',
+  $host           = 'localhost',
+  $server         = 'localhost',
+  $root_user      = $mysql::server::root_user,
+  $root_pass      = $mysql::server::root_pass,
+  $privileges     = 'all privileges',
+  $charset_name   = 'utf8',
+  $collation_name = 'collation_name'
 
 ){
   include mysql
@@ -14,7 +16,7 @@ define mysql::db (
   exec {
     "create_db_$name":
       path        => '/usr/bin:/bin:/sbin',
-      command     => "mysqladmin -u${root_user} -p$root_pass -h \"$server\" create \"$db\"",
+      command     => "mysql -u${root_user} -p$root_pass -h \"$server\" -e \"CREATE DATABASE $db CHARACTER SET $charset_name COLLATE $collation_name\"",
       unless      => "mysql -u${root_user} -p$root_pass -h \"$server\" -e 'use $db'";
   }
   if $user != 'root' {
